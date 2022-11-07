b77713d3f65d3f877f6d7db3011344ba0ed8fd68
Mitch Blackburn <blackburnm2003@gmail.com>
2022-11-07 11:40:30 +0100

    Color functions and button added to JS

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
-const squares = document.querySelector("#squares");
+const squares = document.querySelectorAll('.squares');
+
+const blackBtn = document.querySelector('#blackBtn');
+const colorBtn = document.querySelector('#randomColor');
+
+
+blackBtn.addEventListener('click', () => {
+    for (let square of squares) {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        })
+    }
+});
+
+colorBtn.addEventListener('click', () => {
+    for (let square of squares) {
+        square.addEventListener('mouseover', () => {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        })
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
 
